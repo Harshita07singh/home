09fcebb60f40c845e4b2b7fa26e9d4df4636bd9c
harshita singh <harshi07singh@gmail.com>
2025-10-08 15:57:35 +0530

    commit

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -23,31 +23,27 @@ export const createBlog = async (req, res) => {
 };
 
 export const getBlogs = async (req, res) => {
-  // logic to fetch blogs
-
   try {
-    const { name, title, desc, img, subCategory } = req.body;
-    const newBlog = await blogModel.create({
-      name,
-      title,
-      desc,
-      img,
-      subCategory,
-    });
+    const { name, title, subCategory } = req.query;
 
-    res
-      .status(201)
-      .json({ message: "Blog data created successfully", blog: newBlog });
+    const filter = {};
+    if (name) filter.name = name;
+    if (title) filter.title = title;
+    if (subCategory) filter.subCategory = subCategory;
+
+    const blogs = await blogModel.find(filter);
+
+    res.status(200).json({ message: "Blogs fetched successfully", blogs });
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch blog data" });
   }
 };
 
 export const updateBlog = async (req, res) => {
-  // logic to update a blog by ID
   try {
     const { name, title, desc, img, subCategory } = req.body;
-    const newBlog = await blogModel.create({
+
+    const newBlog = await blogModel.updateOne({
       name,
       title,
       desc,
@@ -57,17 +53,18 @@ export const updateBlog = async (req, res) => {
 
     res
       .status(201)
-      .json({ message: "Blog data created successfully", blog: newBlog });
+      .json({ message: "Blog data updated successfully", blog: newBlog });
   } catch (error) {
+    console.error("Error creating blog:", error);
     res.status(500).json({ error: "Failed to update blog data" });
   }
 };
 
 export const deleteBlog = async (req, res) => {
-  // logic to delete a blog by ID
   try {
     const { name, title, desc, img, subCategory } = req.body;
-    const newBlog = await blogModel.create({
+
+    const newBlog = await blogModel.deleteOne({
       name,
       title,
       desc,
@@ -77,8 +74,9 @@ export const deleteBlog = async (req, res) => {
 
     res
       .status(201)
-      .json({ message: "Blog data created successfully", blog: newBlog });
+      .json({ message: "Blog data deleted successfully", blog: newBlog });
   } catch (error) {
+    console.error("Error creating blog:", error);
     res.status(500).json({ error: "Failed to delete blog data" });
   }
 };
